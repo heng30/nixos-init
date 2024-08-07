560e7dba587e63ca0f2129f3d9ba9e7f8238fe1b
heng30 <2238288979@qq.com>
2024-08-07 23:19:08 +0800

    [*] update normally

--- dwm-scripts/dwm-bg-task/ping_blog.sh
+++ dwm-scripts/dwm-bg-task/ping_blog.sh
@@ -3,7 +3,7 @@
 ret=`curl https://heng30.xyz/ping`
 
 if [ "$ret" == "pong" ]; then
-    echo "O" > "/tmp/ping_blog.dat"
+    echo "  " > "/tmp/ping_blog.dat"
 else
-    echo "E" > "/tmp/ping_blog.dat"
+    echo "" > "/tmp/ping_blog.dat"
 fi;
